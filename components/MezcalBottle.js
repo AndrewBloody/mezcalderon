140cc3f7015365da5d4a1b2f40d1d6ab17f1a367
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2020-07-06 21:26:22 -0500

    Mezcal Bottle component added

--- components/MezcalBottle.js
+++ components/MezcalBottle.js
@@ -1,5 +1,75 @@
+import { useState, useEffect, useRef } from "react";
+import {
+  Canvas,
+  useLoader,
+  useFrame,
+  extend,
+  useThree,
+} from "react-three-fiber";
+
+let GLTFLoader;
+let OrbitControls;
+
+const Gun = () => {
+  const bottleRef = useRef();
+  const [model, setModel] = useState();
+  useFrame(() => {
+    if (bottleRef.current) {
+      bottleRef.current.rotation.y += 0.2;
+    }
+  });
+  GLTFLoader = require("three/examples/jsm/loaders/GLTFLoader").GLTFLoader;
+  useEffect(() => {
+    new GLTFLoader().load("/botella_mezcalderon.gltf", setModel);
+  }, []);
+
+  return model ? <primitive object={model.scene} ref={bottleRef} /> : null;
+};
+
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+  const [hasOrbit, setHasOrbit] = useState(false);
+  useFrame(() => {
+    if (orbitRef.current) {
+      orbitRef.current.update();
+    }
+  });
+  useEffect(() => {
+    OrbitControls = require("three/examples/jsm/controls/OrbitControls")
+      .OrbitControls;
+    extend({ OrbitControls });
+    setHasOrbit(true);
+  }, []);
+  return hasOrbit ? (
+    <orbitControls args={[camera, gl.domElement]} ref={orbitRef} />
+  ) : null;
+};
+
+const Box = () => {
+  const mesh = useRef();
+  useFrame(() => {
+    mesh.current.rotation.y += 0.1;
+    mesh.current.rotation.x += 0.1;
+  });
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshPhysicalMaterial attach="material" color="green" />
+    </mesh>
+  );
+};
+
 const MezcalBottle = () => {
-  return <div>Bottle</div>;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Controls />
+      {/* <Box /> */}
+      <Gun />
+    </Canvas>
+  );
 };
 
 export default MezcalBottle;
